b89ff58b66180fe8bf1c75b368bc61d48e4be242
Sreejith <sreejith.r44@gmail.com>
2014-07-16 00:37:17 +0530

    More polish added to the select_options() function

--- wep_crack.sh
+++ wep_crack.sh
@@ -6,21 +6,38 @@ header() {
 }
 
 
+scan_access_point() {
+    # TODO
+    echo "Displaying access points"
+}
+
+
+crack_access_point() {
+    # TODO
+}
+
+
 select_options() {
     echo "[!] Please select an option below to get started.\n\n"
-    echo "1) Scan access points nearby and crack\n"
-    echo "2) Crack access point with BSSID\n"
+    echo "1) Scan access points nearby and crack"
+    echo "2) Crack access point with BSSID"
+    echo "\nEnter your option: "
     read user_option
-    
-    # Do according to user's selection
+
     if [[ "$user_option" = "1" ]];
         then
         header
-        scan_and_crack
+        echo "Scanning nearby access points..."
+        # This makes the user select an access point and sets its BSSID in
+        # $required_bssid
+        scan_access_point
+        crack_access_point $required_bssid
     elif [[ "$user_option" = "2" ]];
         then
         header
-        crack_access_point
+        echo "Enter BSSID of the access point:"
+        read $required_bssid
+        crack_access_point $required_bssid
     fi
 }
 
