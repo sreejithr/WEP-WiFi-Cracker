120045ec89a26ce8a73de0bc0919e12d602287be
Sreejith <sreejith.r44@gmail.com>
2014-07-16 00:19:41 +0530

    Wrote the header and started working on the options

--- wep_crack.sh
+++ wep_crack.sh
@@ -1,4 +1,22 @@
+header() {
+    echo "========================================\n"
+    echo "WEP encrypted Wi-Fi Access Point Cracker\n"
+    echo "========================================\n"    
+}
+
+select_options() {
+    echo "[!] Please select an option below to get started.\n\n"
+    echo "1) Scan access points nearby and crack\n"
+    echo "2) Crack access point with BSSID\n"
+    read user_option
+    
+    # Do according to user's selection
+    if 
+}
+
 # Display header and options
+header
+select_options
 
 # Let user select an interface
 
